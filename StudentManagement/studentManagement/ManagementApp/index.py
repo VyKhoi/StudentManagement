1f0da6eb27f93178f47f08c7439fc8c8ce9481f6
Dang Vy Khoi <dangvykhoi@gmail.com>
2022-11-24 10:20:31 +0700

    upadte tao trang chu

--- StudentManagement/studentManagement/ManagementApp/index.py
+++ StudentManagement/studentManagement/ManagementApp/index.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+from flask import render_template, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
