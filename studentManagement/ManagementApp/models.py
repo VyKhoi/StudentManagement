6cec3576fd5d586fb1f33ef931edddcd1427cee3
Dang Vy Khoi <dangvykhoi@gmail.com>
2022-12-01 21:08:02 +0700

    dịt cu khôi phục model

--- studentManagement/ManagementApp/models.py
+++ studentManagement/ManagementApp/models.py
@@ -1,15 +1,9 @@
-
 from sqlalchemy.orm import relationship, backref
 from enum import Enum as UserEnum
 from sqlalchemy import Column, Integer, String, Float, Text, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from StudentManagement.studentManagement.ManagementApp import app, db,module_get_last_id
 import re
-
-
-
-# Phần code funtion chung cho model
-
 
 
 class UserRole(UserEnum):
@@ -20,7 +14,7 @@ class UserRole(UserEnum):
 class BaseModel(db.Model):
     __abstract__ = True
 
-
+    id = Column(String(50), primary_key=True)
     name = Column(String(50), nullable=False)
     gender = Column(Boolean, default=False)
     identity = Column(String(50), nullable=False)
@@ -31,17 +25,15 @@ class BaseModel(db.Model):
 
 
 class User(BaseModel):
-
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_user)
+    id = Column(String(50), primary_key=True, default=res)
 
-    id = Column(String(50), primary_key=True,default= res)
 
     username = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
     image = Column(String(50), nullable=False)
     active = Column(String(50), nullable=False)
-
 
 
     news = relationship('News', backref='User', lazy=True)
@@ -59,47 +51,39 @@ class State(db.Model):
 
     student = relationship('Student', backref='State', lazy=True)
 
-    def __str__(self):
-        return self.state
 
 class Student(BaseModel):
     __tablename__ = 'student'
-
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_student)
-
-    id = Column(String(50), primary_key=True, default = res)
+    id = Column(String(50), primary_key=True, default=res)
 
     status = Column(String(50), ForeignKey(State.state), nullable=False)
     score = relationship('Score', backref='Student', lazy=True)
-
+    student_class_school_year = relationship('Student_Class_SchoolYear', backref='Student', lazy=True)
     review = relationship('Reviews', backref='Student', lazy=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Class(db.Model):
     __tablename__ = 'class'
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
-                          module_get_last_id.last_id_class)
+                            module_get_last_id.last_id_class)
     id = Column(String(50), primary_key=True,  default= res)
+
+
     name_class = Column(String(50), nullable=False)
 
 
     teaching_class = relationship('Teaching_Class', backref='Class', lazy=True)
-
-
+    student_class_school_year = relationship('Student_Class_SchoolYear', backref='Class', lazy=True)
     id_teacher_in_charge = Column(String(50), ForeignKey(User.id), nullable=False)
     id_school_year = Column(String(50), ForeignKey('school_year.id'), nullable=False)
-    students = relationship('Student', secondary='student_class', lazy='subquery',
-                        backref=backref('class', lazy=True))
 
 class Reviews(db.Model):
     __tablename__ = 'reviews'
+
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_reviews)
-
     id = Column(String(50), primary_key=True,default = res)
 
     conduct = Column(String(30), nullable=False)
@@ -132,10 +116,10 @@ class Type_Score(db.Model):
 
 class Subjects(db.Model):
     __tablename__ = 'subjects'
+
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_subjects)
     id = Column(String(50), primary_key=True,default = res)
-
     name_subject = Column(String(50), nullable=False)
 
     score = relationship('Score', backref='Subjects', lazy=True)
@@ -151,8 +135,7 @@ class School_Year(db.Model):
     __tablename__ = 'school_year'
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_school_year)
-    id = Column(String(50), primary_key=True, default = res)
-
+    id = Column(String(50), primary_key=True,default = res)
     name = Column(String(50), nullable=True)
     year_start = Column(String(30), nullable=False)
     year_end = Column(String(30), nullable=False)
@@ -160,6 +143,8 @@ class School_Year(db.Model):
 
 
     score = relationship('Score', backref='School_Year', lazy=True)
+    teaching_class = relationship('Teaching_Class', backref='School_Year', lazy=True)
+    student_class_school_year = relationship('Student_Class_SchoolYear', backref='School_Year', lazy=True)
     review = relationship('Reviews', backref='School_Year', lazy=True)
     class_in_year = relationship('Class', backref='School_Year', lazy=True)
 
@@ -168,16 +153,25 @@ class School_Year(db.Model):
 
 class Teaching_Class(db.Model):
     __tablename__ = 'teaching_class'
-
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_teaching_class)
     id = Column(String(50), primary_key=True,default = res)
 
     id_teacher = Column(String(50), ForeignKey(User.id), nullable=False)
     id_class = Column(String(50), ForeignKey(Class.id), nullable=False)
-    id_subject = Column(String(50), ForeignKey(Subjects.id),nullable=False)
+    id_school_year = Column(String(50), ForeignKey(School_Year.id), nullable=False)
+    id_subject = Column(String(50), ForeignKey(Subjects.id), nullable=False)
 
 
+class Student_Class_SchoolYear(db.Model):
+    res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
+                 module_get_last_id.last_id_student_class__school_year)
+
+    id = Column(String(50), primary_key=True,default = res)
+
+    id_student = Column(String(50), ForeignKey(Student.id), nullable=False)
+    id_class = Column(String(50), ForeignKey(Class.id), nullable=False)
+    id_school_year = Column(String(50), ForeignKey(School_Year.id), nullable=False)
 
 
 # class nài dùng để cấu trúc tin tức
@@ -192,7 +186,6 @@ class Role(db.Model):
 
 class Permission(db.Model):
     __tablename__ = 'permission'
-
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_permission)
     id = Column(String(50), primary_key=True, nullable=False,default = res)
@@ -206,7 +199,6 @@ role_permission = db.Table('role_permission',
     Column('role', String(50), ForeignKey(Role.role), primary_key=True, nullable=False),
     Column('id_per', String(50), ForeignKey(Permission.id), primary_key=True, nullable=False))
 
-
 user_role = db.Table('user_role',
     Column('role', String(50), ForeignKey(Role.role), primary_key=True, nullable=False),
     Column('id_user', String(50), ForeignKey(User.id), primary_key=True, nullable=False))
@@ -215,16 +207,15 @@ user_subject = db.Table('user_subject',
     Column('id_user', String(50), ForeignKey(User.id), primary_key=True, nullable=False),
     Column('id_sub', String(50), ForeignKey(Subjects.id), primary_key=True, nullable=False))
 
-Student_Class = db.Table('student_class',
-    Column('id_student',String(50), ForeignKey(Student.id),primary_key=True, nullable=False),
-    Column('id_class',String(50), ForeignKey(Class.id),primary_key=True, nullable=False))
+
+
 
 
 class News(db.Model):
     __tablename__ = 'news'
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_news)
-    id = Column(String(30), primary_key=True, nullable=False, default =res)
+    id = Column(String(30), primary_key=True, nullable=False, default = res)
     header = Column(String(1000), nullable=False)
     content = Column(String(7000), nullable=False)
     date_push = Column(String(50), nullable=False)
@@ -247,25 +238,15 @@ class RulesTable(db.Model):
 class Rule(db.Model):
     res = re.sub(r'[0-9]+$', lambda x: f"{str(int(x.group()) + 1).zfill(len(x.group()))}",
                  module_get_last_id.last_id_rule)
-    id = Column(String(50), primary_key=True, nullable=False, default = res)
+    id = Column(String(50), primary_key=True, nullable=False,default = res)
     name = Column(String(50), nullable=False)
     value = Column(String(50), nullable=False)
     id_rules_table = Column(String(50), ForeignKey(RulesTable.id), nullable=False)
 
 
-
-
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # c = Teaching_Class.query.get('t-c:1')
-        # print(c.Class.id_school_year)
-        # p = School_Year.query.get(c.Class.id_school_year)
-        # print(p.name)
-        # p = User.query.get('u1')
-        # print( p.Subjects[0].name_subject )
-        # print( get_last_id(Class))
-        # # c = News.query.get('n:00001')
-        # # print(c.User.name)
-        # #khoi da commit
+        # c = News.query.get('n:00001')
+        # print(c.User.name)
+        #khoi da commit
