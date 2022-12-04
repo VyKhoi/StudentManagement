60fa97b1fd87f36740d46bbb3eda946e5f2b3e7c
Dang Vy Khoi <dangvykhoi@gmail.com>
2022-12-04 21:05:11 +0700

    xử lý chức năng register,login,404,401

--- studentManagement/ManagementApp/__init__.py
+++ studentManagement/ManagementApp/__init__.py
@@ -4,8 +4,15 @@ from urllib.parse import quote
 from flask_babelex import Babel
 import cloudinary
 from flask_admin import Admin, BaseView, expose
-app = Flask(__name__)
 
+from flask_login import LoginManager
+app = Flask(__name__)
+cloudinary.config(
+     cloud_name = 'dendeb697',
+            api_key = '822515132138445',
+            api_secret = 'bBck-UodqY0PZqDOOIjL2nBUHwc'
+
+)
 # co so du lieu
 app.secret_key = '4567890sdfghjklcvbnvb4567fg6yug'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/btl_4?charset=utf8mb4' % quote('123456')
@@ -16,7 +23,7 @@ app.config['PAGE_SIZE'] = 3
 
 admin = Admin(app=app, name='Trang quản trị', template_mode='bootstrap4')
 
-
+login = LoginManager(app= app)
 
 # cloudinary.config(cloud_name = "dssijhrst", api_key = "979849377371891", api_secret ="3V_MpBv1G_D-gn1z5VJonRaVX-E")
 # babel = Babel(app=app)
--- studentManagement/ManagementApp/dao.py
+++ studentManagement/ManagementApp/dao.py
@@ -1,20 +1,81 @@
-# "model xử lý những hàm chung chung với db"
-# from StudentManagement.studentManagement.ManagementApp import app
-# from StudentManagement.studentManagement.ManagementApp.models import *
-# import sys
-#
-# def str_to_class(classname):
-#     return getattr(sys.modules[__name__], classname)
-#
-# def get_last_id(table):
-#
-#         cl = str_to_class(table)
-#         p =  cl.query.order_by(cl.id.desc()).first()
-#         return p.id
-#
-#
-#
-# if __name__ == '__main__':
-#         with app.app_context():
-#          print(get_last_id('Class'))
-#          # print(str_to_class('Class'))
+from StudentManagement.studentManagement.ManagementApp.models import *
+from StudentManagement.studentManagement.ManagementApp import db,app
+
+
+import hashlib
+
+# ,hometown,birthday,phone,
+#                    email,username, password,
+#                    image,active):
+def get_user(identity,id = None):
+    if id:
+        return User.query.get(id)
+    if identity:
+        return User.query.filter(User.identity.__eq__(identity)).first()
+    
+    return "404"
+
+def add_user(name,username,password, **kwargs):
+    # password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    print(kwargs.get('birthday'))
+    print(password)
+    user2 = User(name = name,
+                username = username,
+                password = password,
+
+
+             gender= bool(kwargs.get('gender')),
+             identity= kwargs.get('identity'),
+             hometown= kwargs.get('hometown'),
+             birthday= kwargs.get('birthday'),
+             phone= kwargs.get('phone'),
+             email= kwargs.get('email'),
+
+             image= kwargs.get('image'),
+             active= '1')
+
+
+
+    for i in kwargs.get('role'):
+        # print(i)
+        r = Role.query.get(i)
+        user2.role.append(r)
+    print(user2)
+    db.session.add(user2)
+    db.session.commit()
+
+
+def check_login(username,password):
+    if username and password:
+        password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+        return User.query.filter(User.active.__eq__(1),User.username.__eq__(username.strip()),
+                                 User.password.__eq__(password)).first()
+
+def get_user_id(id):
+    return User.query.get(id)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        # print()
+        # u = get_user(1234)
+        # # u.role = 'teacher'
+        # list = ['teacher', 'school_manager', 'academic_staff']
+        # r = Role.query.filter()
+        #
+        # u.role.append(r)
+        #
+        #
+        # print(u.role)
+        # db.session.add(u)
+        # db.session.commit()
+        u = check_login('vykhoi','123')
+        print(type(u))
+        print(u.birthday)
+        print(u.role[0].role)
+
+        if 'c' == 'c':
+            print("oke")
+
+        # add_role_user('teacher','12345678')
